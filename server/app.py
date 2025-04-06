96d9734430722551d5dcde00772dbc492185e80d
Deepak Kumar <dk607833@gmail.com>
2025-04-06 17:15:32 +0530

    Initial commit

--- server/app.py
+++ server/app.py
@@ -106,4 +106,5 @@ def analyze_image():
     return jsonify({"items": results, "summary": summary})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
